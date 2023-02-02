04d7607ecf8fd7466b0a22fdb2caf886fd2349c7
Andre Dreyer <andre@evalgeniuses.com>
2023-02-02 12:20:31 +0200

    Add key encoding parameter

--- index.js
+++ index.js
@@ -9,22 +9,30 @@
 var compare = require('tsscmp')
 var crypto = require("crypto")
   
-function Keygrip(keys, algorithm, encoding) {
+function Keygrip(keys, algorithm, encoding, keyEncoding) {
   if (!algorithm) algorithm = "sha1";
   if (!encoding) encoding = "base64";
-  if (!(this instanceof Keygrip)) return new Keygrip(keys, algorithm, encoding)
+  if (!(this instanceof Keygrip)) return new Keygrip(keys, algorithm, encoding, keyEncoding)
 
   if (!keys || !(0 in keys)) {
     throw new Error("Keys must be provided.")
   }
 
   function sign(data, key) {
-    return crypto
-      .createHmac(algorithm, key)
-      .update(data).digest(encoding)
+    var chain = crypto;
+    if (keyEncoding) {
+      chain = chain.createHmac(algorithm, key, {encoding: keyEncoding});
+    } else {
+      chain = chain.createHmac(algorithm, key);
+    }
+    chain = chain.update(data).digest(encoding)
+    if (encoding === "base64") {
+      return chain
       .replace(/\/|\+|=/g, function(x) {
         return ({ "/": "_", "+": "-", "=": "" })[x]
       })
+    }
+    return chain;
   }
 
   this.sign = function(data){ return sign(data, keys[0]) }
